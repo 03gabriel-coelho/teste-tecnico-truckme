d42510d0efd394651256e86f3931d86f640e7a34
Gabriel Freitas <gabriel.freitas@in8.com.br>
2024-07-14 18:59:30 -0300

    feat: gallery component created

---
+++ web-framework/src/components/GalleryComponent.vue
@@ -0,0 +1,63 @@
+<script setup>
+defineProps({})
+</script>
+
+<template>
+  <footer class="footer-carousel">
+    <button type="button" id="carousel-back">Anterior</button>
+    <div>
+      <img
+        id="carousel-image"
+      />
+    </div>
+    <button type="button" id="carousel-next">Próximo</button>
+  </footer>
+</template>
+
+<style scoped>
+.footer-carousel {
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.footer-carousel div {
+  width: 100vw;
+  height: 400px;
+  overflow: hidden;
+  position: relative;
+}
+
+.footer-carousel button {
+  display: inline-block;
+  position: absolute;
+}
+
+.footer-carousel button:first-of-type  {
+  left: 5.7vw;
+  z-index: 1;
+}
+
+.footer-carousel button:last-of-type  {
+  right: 5vw;
+}
+
+.footer-carousel img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: center;
+}
+
+@media (max-width: 700px) {
+  .footer-carousel {
+    align-items: end;
+  }
+
+  .footer-carousel button {
+    margin-bottom: 20px;
+  }
+}
+</style>
--- web-framework/src/views/HomeView.vue
+++ web-framework/src/views/HomeView.vue
@@ -2,7 +2,8 @@
   import BannerComponent from '../components/BannerComponent.vue'
   import AboutComponent from '../components/AboutComponent.vue'
   import ServicesTableComponent from '../components/ServicesTableComponent.vue'
-  import ContactFormComponent from '@/components/ContactFormComponent.vue';
+  import ContactFormComponent from '../components/ContactFormComponent.vue'
+  import GalleryComponent from '../components/GalleryComponent.vue'
 </script>
 
 <template>
@@ -10,4 +11,5 @@
   <AboutComponent />
   <ServicesTableComponent />
   <ContactFormComponent />
+  <GalleryComponent />
 </template>
