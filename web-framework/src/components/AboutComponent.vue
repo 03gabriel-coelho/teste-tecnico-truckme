0997ffb15718f0206d70577c559038b264d0d09d
Gabriel Freitas <gabriel.freitas@in8.com.br>
2024-07-14 18:43:29 -0300

    feat: About created

---
+++ web-framework/src/components/AboutComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({})
+</script>
+
+<template>
+  <section class="about" id="about">
+    <h1 class="title">Sobre nós</h1>
+    <p>
+      Através da nossa Plataforma Web, garantimos serviço com todos os
+      requisitos e obrigações da transportadora tradicional.
+    </p>
+    <p id="second-paragraph">
+      Conectamos vocês Embarcadores aos mais qualificados transportadores
+      autônomos, através de um criterioso processo de gestão e simplificação
+      de processos do frete!
+    </p>
+    <button id="button-expand-text">Leia mais!</button>
+  </section>
+</template>
+
+<style scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+}
+
+.about p {
+  font-size: 18px;
+}
+
+.about #second-paragraph {
+  display: none;
+}
+
+</style>
--- web-framework/src/views/HomeView.vue
+++ web-framework/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
   import BannerComponent from '../components/BannerComponent.vue'
+  import AboutComponent from '../components/AboutComponent.vue'
 </script>
 
 <template>
   <BannerComponent />
+  <AboutComponent />
 </template>
